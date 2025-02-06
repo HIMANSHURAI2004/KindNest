5906cec5b158d245fa1d65a28f4288cb77891ab0
HIMANSHU-RAI <131878420+HIMANSHURAI2004@users.noreply.github.com>
2025-02-07 01:03:36 +0530

    Refactor layout and styling for SignUp screen

--- app/login/SignUp.tsx
+++ app/login/SignUp.tsx
@@ -2,11 +2,11 @@ import { View, Text, TextInput, TouchableOpacity, ToastAndroid, Alert } from 're
 import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import Colors from '@/constants/Colors'
-import { useRouter } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import {auth} from '@/config/FirebaseConfig'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { setLocalStorage } from '@/service/Storage'
-
+import AntDesign from '@expo/vector-icons/AntDesign';
 const SignUp = () => {
 
     const router = useRouter()
@@ -45,100 +45,131 @@ const SignUp = () => {
     }
 
   return (
-<View style= {{
-        padding: 25,
-    }}>
-      <Text style={styles?.textHeader}>Let's Sign You Up !</Text>
-      <Text style={styles?.subText}>Welcome</Text>
-      {/* <Text style={styles?.subText}>You've been missed</Text> */}
-      <View style={{marginTop: 25}}>
+  <View
+  className='h-full w-full flex justify-center items-center'
+  >
+    <View className='h-[30%] rounded-b-3xl w-full relative'
+      style = {{
+        backgroundColor: Colors.PRIMARY,
+      }}
+    >
+      <View className='absolute bottom-2 flex justify-center w-full px-10 py-4'>
+        <Text className='text-2xl font-bold text-left py-2 text-white font-[poppins]'>Create an account</Text>
+        <Text className='text-left py-1 pr-4 text-slate-300 font-[poppins]'>Welcome! Please enter your details.</Text>
+      </View>
+    </View>
+    <View className='h-[70%] w-full bg-white px-8 pt-5'>
+      <TouchableOpacity
+          className='flex  justify-center items-center w-full py-4 gap-2'
+          style = {{
+            display: 'flex',
+            flexDirection: 'row',
+            padding: 15,
+            backgroundColor: 'white',
+            borderRadius: 10,
+            marginTop: 20,
+            borderWidth: 1,
+            borderColor: Colors.PRIMARY,
+          }}
+          onPress={() => router.push('/login/SignUp')
+        }>
+          <AntDesign name="google" size={24} color="teal" />
+          <Text style= {{
+              textAlign: 'center',
+              fontSize: 16,
+              color: Colors.PRIMARY
+          }}
+          className='font-[poppins]'
+          >Sign up with google</Text>
+      </TouchableOpacity>
 
-      <Text>Full Name</Text>
+      <View className='flex justify-center items-center w-full pt-2 pb-2'
+        style = {{
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <View className='h-[1px] bg-gray-300 w-[25%]' />
+        <Text className='font-medium py-1 px-2 text-slate-500 font-[poppins]'>or</Text>
+        <View className='h-[1px] bg-gray-300 w-[25%]' />
+      </View>
+
+      <View>
+
+        <Text>Full Name</Text>
         <TextInput
           style={styles?.textInput}
           placeholder="Enter your full name"
           onChangeText={(value) => setUserName(value)}
-          />
-        <Text>Email</Text>
+        />
+
+        <Text className='font-medium pt-1 pb-0.5 pl-1 text-slate-700 font-[poppins]'>Email</Text>
         <TextInput
           style={styles?.textInput}
           placeholder="Enter your email"
           onChangeText={(value) => setEmail(value)}
-          />
+          className='font-[poppins]'
 
-        <Text>Password</Text>
+          />
+        <Text className=' font-medium pt-1 pb-0.5 pl-1 text-slate-700 font-[poppins]'>Password</Text>
         <TextInput
           style={styles?.textInput}
           placeholder="Enter your password"
           secureTextEntry={true}
           onChangeText={(value) => setPassword(value)}
+          className='font-[poppins]'
           />
       </View>
 
       <TouchableOpacity style={styles?.button}
-        onPress={OnCreateAccount}
+      onPress={OnCreateAccount}
+      className='font-[poppins]'
       >
         <Text style= {{
             textAlign: 'center',
             fontSize: 16,
             color: 'white'
-        }}>Sign Up</Text>
+        }}>Sign up</Text>
       </TouchableOpacity>
       
-
-      <TouchableOpacity style={styles?.buttonCreate
-        } onPress={() => router.push('/login/SignIn')
-      }>
-        {/* <Text>Don't have an account?</Text> */}
-        <Text style= {{
+        <Link href='/login/SignIn' style= {{
             textAlign: 'center',
-            fontSize: 16,
+            fontSize: 13,
             color: Colors.PRIMARY
-        }}>Already have an account? Sign In</Text>
-      </TouchableOpacity>
+        }}
+        className='underline font-[poppins] pt-2'
+        >Already have an account? Sign in</Link>
     </View>
-  )
-}
+  </View>
+    )
+  }
 
-const styles = StyleSheet.create({
-    textHeader: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: Colors.PRIMARY,
-        textAlign: 'center',
-    },
-    subText: {
-        color: Colors.GRAY,
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginTop: 10,
-        textAlign: 'center'
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: 'black',
-        padding: 10,
-        fontSize: 16,
-        borderRadius: 10,
-        marginTop: 5,
-        backgroundColor: 'white',
-        marginBottom: 35,
-    },
-    button: {
-        backgroundColor: Colors.PRIMARY,
-        padding: 15,
-        borderRadius: 10,
-        marginTop: 5,
-        color: 'white',
-    },
-    buttonCreate: {
-        padding: 15,
-        backgroundColor: 'white',
-        borderRadius: 10,
-        marginTop: 15,
-        borderWidth: 1,
-        borderColor: Colors.PRIMARY,
-    }
-})
+  const styles = StyleSheet.create({
+      textInput: {
+              borderWidth: 0.5,
+              borderColor: 'black',
+              padding: 8,
+              fontSize: 14,
+              borderRadius: 10,
+              marginTop: 5,
+              backgroundColor: 'white',
+              marginBottom: 20,
+          },
+          button: {
+              backgroundColor: Colors.PRIMARY,
+              padding: 15,
+              borderRadius: 10,
+              marginTop: 5,
+              color: 'white',
+          },
+          buttonCreate: {
+              padding: 15,
+              backgroundColor: 'white',
+              borderRadius: 10,
+              marginTop: 15,
+              borderWidth: 1,
+              borderColor: Colors.PRIMARY,
+          }
+  })
 
-export default SignUp
+  export default SignUp
